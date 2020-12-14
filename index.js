d8fbe71bde6ea9d859d0884c2914868c417b979f
Lewis Chandler <lewis137@hotmail.com>
2020-12-14 15:43:09 +0000

    added moves

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const grid = document.querySelector('.grid')
 const startButton = document.querySelector('#start') // or document.getElementById('start')
 const scoreDisplay = document.querySelector('#score') // or document.getElementById('score')
 const highScoreDisplay = document.querySelector('#highScore')
+const movesDisplay = document.querySelector('#moves')
 const classicButton = document.querySelector('#classicMode')
 const sleekButton = document.querySelector('#sleekMode')
 let squares = []
@@ -9,12 +10,14 @@ let currentSnake = [2,1,0]
 let direction = 1
 const width = 10
 let appleIndex = 0
+let moves = 0
 let score = 0
 let highScore = 0 
 let intervalTime = 750
 let speedChange = 0.95
 let timerId = 0
 var snake = 'classicSnake'
+
 
 function createGrid() {
     for (let i = 0; i < width*width; i++){
@@ -44,12 +47,15 @@ function changeTheme2() {
 
 
 function startGame() {
-
+    
+    score = 0
+    moves = 0
     currentSnake.forEach(index => squares[index].classList.remove(snake))
     squares[appleIndex].classList.remove('apple')
     clearInterval(timerId)
     currentSnake = [2,1,0]
-    score = 0
+    
+    movesDisplay.textContent = moves
     scoreDisplay.textContent = score
     direction = 1
     intervalTime = 1000
@@ -57,9 +63,11 @@ function startGame() {
     currentSnake.forEach(index => squares[index].classList.add(snake))
     generateApples()
     timerId = setInterval(move, intervalTime)
+    
 }
 
 function move() {
+    
     if (
         (currentSnake[0] + width >= width*width && direction === width) || // bottom
         (currentSnake[0] % width === width-1 && direction === 1) || // right
@@ -69,10 +77,12 @@ function move() {
 
     ) 
     return clearInterval(timerId)
+    
 
     const tail = currentSnake.pop()
     squares[tail].classList.remove(snake)
     currentSnake.unshift(currentSnake[0] + direction)
+    movesDisplay.textContent = moves
     if (squares[currentSnake[0]].classList.contains('apple')){
         squares[tail].classList.add(snake)
         currentSnake.push(tail)
@@ -87,6 +97,7 @@ function move() {
         }
         scoreDisplay.textContent = score
         highScoreDisplay.textContent = highScore
+        
         clearInterval(timerId)
         intervalTime = intervalTime * speedChange
         timerId = setInterval(move, intervalTime)
@@ -105,18 +116,28 @@ function generateApples() {
     squares[appleIndex].classList.add('apple')
 }
 
-generateApples()
+
 
 function control(e) {
     if (e.keyCode === 39){
-        direction = 1 // right
+        moves++
+        direction = 1
+        // right
     } else if (e.keyCode === 38) {
-        direction = -width // up
+        moves++
+        direction = -width
+         // up
     } else if (e.keyCode === 37){
-        direction = -1 // left
+        moves++
+        direction = -1
+         // left
     } else if (e.keyCode === 40) {
-        direction = width // down
-    }
+        moves++
+        direction = width
+         // down
+    } 
+       
+    
 }
 
 document.addEventListener('keydown', control)
